c4da08941874de310886a5ed20f4003901c79dab
adriani94bil <adrian.ruiz.donado@gmail.com>
2024-06-02 23:44:42 +0200

    Univariante

---
+++ pickle.py
@@ -0,0 +1,8 @@
+# Básicos
+# ==============================================================================
+
+import pandas as pd
+import numpy as np
+import pickle
+import datetime as dt
+import scipy
--- utils/funciones.py
+++ utils/funciones.py
@@ -3,10 +3,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris 
 import seaborn as sns
-from utils import variables as var
-from sklearn.linear_model import LinearRegression
 import yfinance as yf
+
+##Función para rellenar nulos en los datasets de acciones, índices
 
 def fill_null(df:pd, tkr, col, missing):
     ticker = yf.Ticker(tkr)
     df.loc[df['Symbol']==tkr, col]= ticker.info[missing]
+    
+    ##Generador de reportes de datos
+    
+def data_report(df):
+    # Sacamos los NOMBRES
+    cols = pd.DataFrame(df.columns.values, columns=["COL_N"])
+
+    # Sacamos los TIPOS
+    types = pd.DataFrame(df.dtypes.values, columns=["DATA_TYPE"])
+
+    # Sacamos los MISSINGS
+    percent_missing = round(df.isnull().sum() * 100 / len(df), 2)
+    percent_missing_df = pd.DataFrame(percent_missing.values, columns=["MISSINGS (%)"])
+
+    # Sacamos los VALORES UNICOS
+    unicos = pd.DataFrame(df.nunique().values, columns=["UNIQUE_VALUES"])
+    
+    percent_cardin = round(unicos['UNIQUE_VALUES']*100/len(df), 2)
+    percent_cardin_df = pd.DataFrame(percent_cardin.values, columns=["CARDIN (%)"])
+
+    concatenado = pd.concat([cols, types, percent_missing_df, unicos, percent_cardin_df], axis=1, sort=False)
+    concatenado.set_index('COL_N', drop=True, inplace=True)
+
+
+    return concatenado.T
+
+## La función inversa de boxcox
+
+def inverse_boxcox(y, lambda_):
+    return np.exp(y) if lambda_ == 0 else np.exp(np.log(lambda_ * y + 1) / lambda_)
